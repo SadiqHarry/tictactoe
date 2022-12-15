3d0644a1c0fb535a8ac3687347c4ec078b9dd84f
SadiqHarry <sadiqharry@gmail.com>
2022-12-15 18:35:42 -0500

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -5,6 +5,8 @@ board =  ["-", "-", "-",
 currentplayer = "X"
 winner = None 
 gamerunning = True
+has_lost = True
+
 
 #print game board
 
@@ -14,29 +16,28 @@ def printboard(board):
     print(board[3] + " | " + board[4] + " | " + board[5])
     print("----------")
     print(board[6] + " | " + board[7] + " | " + board[8])
-printboard(board)
 
 
 #input move
 def playerinput(board):
     inp = int(input("Enter a number 1-9: "))
+    #If there is a number from 1-9 check to see if there is a blank spot("-") by -1 because input is indexed at 1
     if 1 <= inp <= 9 and board[inp-1] == "-":
-        #If there is a number from 1-9 check to see if there is a blank spot("-") by -1 because the board is indexed 0 and input is indexed 1
+                #If there is a blank spot("-") insert current_player("X")
         board[inp-1] = currentplayer
-        #If there is a blank spot("-") incser current_player("X")
     else:
         print("Sorry, spot is taken")
 
 #check for win or tie
 
 def checkrow(board):
-    global winner
     #global editing of winner
+    global winner
+    #If all values in row are equal and not "-" print winner
     if board[0] == board[1] == board[2] and board[0] != "-":
-        #If all values in row equals are equal and not "-" print winner
         winner = board[0]
-        return True
         #Using boolean so that we can efficently break out of loops
+        return True
     elif board[3] == board[4] == board[5] and board[3] != "-":
         winner = board[3]
         return True
@@ -67,14 +68,41 @@ def checkdiagonal(board):
 
 
 
+def checkwin():
+    if checkcolumn(board) or checkdiagonal(board) or checkrow(board):
+        print(f"The winner is {winner}")
+        has_lost = False
+        
+
+
+
+def checktie(board):
+    
+    global gamerunning
+    #If no winner(has_lost) + no blank spaces = tie
+    if "-" not in board and has_lost == False:
+        printboard(board)
+        print("It is a tie!")
+        gamerunning = False
+
+
+
 #switch player
+
+def switchplayer():
+    global currentplayer
+    if currentplayer == "X":
+        currentplayer = "O"
+    else:
+        currentplayer = "X"
 
 #check for win or tie again
 
-while gamerun:
+
+while gamerunning:
     printboard(board)
     playerinput(board)
-
-
-
-#
+    checkwin()
+    checktie(board)
+    switchplayer()
+ 
